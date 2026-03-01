ace401bbbe12556d29e6961d291ce9bf1f641e9a
MichaelT-W23 <michael.totaro@icloud.com>
2026-03-01 01:02:29 -0500

    Revert apolloClient test

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -1,24 +1,24 @@
-// import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-// const graphqlEndpoint = 'https://www.general-api.com/bookql/graphql';
-
-// const apolloClient = new ApolloClient({
-//   uri: graphqlEndpoint,
-//   cache: new InMemoryCache(),
-// });
-
-// export default apolloClient;
-
-
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core';
-
-const httpLink = new HttpLink({
-  uri: 'https://www.general-api.com/bookql/graphql',
-});
+const graphqlEndpoint = 'https://www.general-api.com/bookql/graphql';
 
 const apolloClient = new ApolloClient({
-  link: httpLink,
+  uri: graphqlEndpoint,
   cache: new InMemoryCache(),
 });
 
 export default apolloClient;
+
+
+// import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core';
+
+// const httpLink = new HttpLink({
+//   uri: 'https://www.general-api.com/bookql/graphql',
+// });
+
+// const apolloClient = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache(),
+// });
+
+// export default apolloClient;
