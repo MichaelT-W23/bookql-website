e60e1637541fa40b038bba1cec01dbb61a1b321e
MichaelT-W23 <michael.totaro@icloud.com>
2026-03-29 18:47:50 -0400

    Updated vite.config.js server

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
 	base: '/',
 	plugins: [vue()],
+	resolve: {
+    alias: {
+      '@': new URL('./src', import.meta.url).pathname
+    }
+  },
+  server: {
+    host: "localhost", 
+  }
 })
